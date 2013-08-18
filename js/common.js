546ce5d6a619108c4ea7a5c030d7899fde039dfd
Anton Kuznetsov <isnifer@gmail.com>
2013-08-18 16:27:42 +0400

    It's one form, lol. Rearrange code

--- js/common.js
+++ js/common.js
@@ -9,6 +9,15 @@
             $(this).siblings('.b-question__interactive').toggleClass('b-question__interactive_state_hidden');
         });
 
+        // Select offer. View input on another value
+        $('.b-about__select_offer').on('change', function () {
+           if ($(this).val() === 'another') {
+               $('.b-about__label_another-offer').addClass('b-about__label_another-offer_state_visible');
+           } else {
+               $('.b-about__label_another-offer').removeClass('b-about__label_another-offer_state_visible');
+           }
+        });
+
     });
 
 }(jQuery));
